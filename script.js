be9f98dbab5c021386cfffea2090851d63459e00
codigowebmp <103782811+codigowebmp@users.noreply.github.com>
2024-01-03 17:14:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Este script me permite detectar el click en el link del menu y 
+//agregar las clases correspondientes
+//para que luego con css se produzca el efecto donde se mueve el indicador
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item)=>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((item)=>
+item.addEventListener('click', activeLink));
